ebaad71db38b9fe07fb4893f619efb49c01298f8
ramseypapii <ramsey.kuwona@student.moringaschool.com>
2023-12-14 08:45:35 +0300

    completed assignment

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,11 @@
 
 from user import User
 
+
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, string):
+        self.knowledge.append(string)
